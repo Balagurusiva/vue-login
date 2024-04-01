092849864f7467aca290d20a8107609285f47540
Balaguru <balagurusiva2002@gmail.com>
2024-04-01 17:54:24 +0530

    modified:   nuxt.config.ts 	modified:   package-lock.json 	modified:   package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
-	modules: ["nuxt-mongoose", "@nuxtjs/tailwindcss"],
+	modules: ["nuxt-mongoose", "@nuxtjs/tailwindcss", "@nuxtjs/proxy"],
 	mongoose: {
 		uri: process.env.MONGODB_URI,
 		options: {},
-		modelsDir: "models", 
+		modelsDir: "models",
 	},
-    routeRules: {
-        '/**': {
-            cors: false,
-         },
-    },
+	routeRules: {
+		'/api/**': { cors: true },
+	},
+	 
 });
