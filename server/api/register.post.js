dc0967adfaa7f87ce83535c35977aee15f5a98a6
Balaguru <balagurusiva2002@gmail.com>
2024-04-05 09:36:09 +0530

    modified:   server/api/register.post.js

--- server/api/register.post.js
+++ server/api/register.post.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.model"
 
 export default defineEventHandler(async (event) => {
     try {
-        const { name, email, password } = JSON.parse(await readBody(event))
+        const { name, email, password } = await readBody(event)
         if (name !== "" & email !== "" & password !== "") {
             const user = await User.create({
                 name, email, password
@@ -17,4 +17,24 @@ export default defineEventHandler(async (event) => {
         console.log("error from registration api >>" + error)
         return error
     }
+
+
+
+
+    // try {
+    //     const { name, email, password } = JSON.parse(await readBody(event))
+    //     if (name !== "" & email !== "" & password !== "") {
+    //         const user = await User.create({
+    //             name, email, password
+    //         })
+    //         //return { msg: "login succesfull" }
+    //         return "login succesfull"
+    //     } else {
+
+    //         return "invalid arguments or data missing"
+    //     }
+    // } catch (error) {
+    //     console.log("error from registration api >>" + error)
+    //     return error
+    // }
 })
