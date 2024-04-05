993852883763ea7e47f529b832b968c6608d3c52
Balaguru <balagurusiva2002@gmail.com>
2024-04-05 17:45:40 +0530

    modified:   server/api/register.post.js

--- server/api/register.post.js
+++ server/api/register.post.js
@@ -6,17 +6,12 @@ export default defineEventHandler(async (event) => {
         if (name !== "" & email !== "" & password !== "") {
             const user = await User.create({
                 name, email, password
-            })
-            //return { msg: "login succesfull" }
+            }) 
             event.node.res.setHeader('Access-Control-Allow-Origin', '*');
-            event.node.res.setHeader('Origin, X-Requested-With, Content-Type, Accept, Authorization');
-            if (event.node.req.method === 'OPTIONS') {
-                event.node.res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-                return "login succesfull"
-            } else {
-                event.node.res.setHeader('Access-Control-Allow-Origin', '*');
-                return "invalid arguments or data missing"
-            }
+            return "login successfull"
+        } else {
+            event.node.res.setHeader('Access-Control-Allow-Origin', '*');
+            return "invalid arguments or data missing"
         }
     } catch (error) {
         console.log("error from registration api >>" + error)
